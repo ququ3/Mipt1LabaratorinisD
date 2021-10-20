944858b635fb62e697435f0ca8e88d99c2d734df
ququ3 <dulskasjustas@gmail.com>
2021-10-20 22:21:45 +0300

    update

--- app/src/main/java/com/example/mipt1labaratorinisd/MainActivity.java
+++ app/src/main/java/com/example/mipt1labaratorinisd/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.mipt1labaratorinisd;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tvMain;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.tvMain = findViewById(R.id.tvMain);
+    }
+
+    public void onBtnChangeTextClick(View view) {
+        this.tvMain.setText("Text was changed");
+
     }
 }
